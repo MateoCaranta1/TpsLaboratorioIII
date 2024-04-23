1827c2f3757d2d302f10d6d359e5a9a79d763c23
mateocaranta <mateocaranta@gmail.com>
2024-04-23 15:50:47 -0300

    Tp2Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import './App.css'
 import Tablex from './components/tablex/Tablex';
 
-
-const netIncomes = [
-  { brand: 'McDonalds', income: 1291283 },
-  { brand: 'Burger King', income: 1927361 },
-  { brand: 'KFC', income: 1098463 }
-];
-
 function App() {
+
+  const netIncomes = [
+    { brand: 'McDonalds', income: 1291283 },
+    { brand: 'Burger King', income: 1927361 },
+    { brand: 'KFC', income: 1098463 }
+  ];
   
   return (
     <>
-      <Tablex netIncomes ={netIncomes}/>
+      <Tablex netIncomes = {netIncomes}/>
     </>
   )
 }
